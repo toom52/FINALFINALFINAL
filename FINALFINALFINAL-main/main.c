79ea87e7eda5734516d0315ac838d6ed9a4243f0
francogiusto <franco_giusto@hotmail.com>
2022-06-20 18:31:05 -0300

    Update main.c

--- FINALFINALFINAL-main/main.c
+++ FINALFINALFINAL-main/main.c
@@ -5,6 +5,7 @@
 #include <conio.h>
 #include "consumo.h"
 #include "menu.h"
+#include "cliente.h"
 
 int BuscarConsumo(char ArchivoConsumo[],char fecha[]);
 int BuscaIDCliente(char ArchivoConsumo[],int idC);
@@ -12,6 +13,15 @@ int BuscaIDConsumo(char ArchivoConsumo[],char fecha[],int idC);
 void MostrarConsumoXFECHA();
 void MostrarConsumoXUSUARIO();
 void MostrarConsumoXFyU();
+
+int validarMail(char nombreArchivo[], char email[]);
+int validarNumero(char numero[]);
+int validarDni(char nombreArchivo[], char dni[]);
+int validarMovil(char nombreArchivo[], char movil[]);
+int validarNroCliente(char nombreArchivo[], int nroCliente);
+int validarUnEntero(int num);
+
+
 
 
 int main()
@@ -142,3 +152,154 @@ void MostrarConsumoXFyU()
         muestraArchivoConsumo("consumo.dat",idC,fecha);
     }
 }
+
+
+
+///-----------------------------------------CLIENTES-------------------------------------------------------
+
+
+
+///----------------------------------Validaciones Cliente--------------------------------------------------
+
+int validarMail(char nombreArchivo[], char email[])
+{
+    if(strstr(email, "@") == NULL)
+    {
+        ///yellow();
+        printf("\nEste mail no es valido\n");
+        printf("Intenta con otro mail\n");
+        ///white();
+        return 1;
+    }
+
+    FILE *archivo = fopen(nombreArchivo, "rb");
+    stCliente c;
+    if(archivo)
+    {
+        while(fread(&c, sizeof(stCliente), 1, archivo) > 0)
+        {
+            if(strcmp(c.email, email) == 0)
+            {
+                ///yellow();
+                printf("\nEste mail ya se encuentra registrado\n");
+                printf("Intenta con otro Email\n");
+                ///white();
+                return 1;
+            }
+        }
+        fclose(archivo);
+    }
+    return 0;
+}
+
+
+int validarNumero(char numero[])
+{
+
+    int i=0;
+    int flag=0;
+    int esNumero;
+
+    while(i<strlen(numero)&& flag==0)
+    {
+        esNumero = isdigit(numero[i]);
+        if (esNumero !=0)       /// ingreso al if si esta todo bien, si hasta el momento son numeros
+        {
+            i++;
+        }
+        else
+        {
+            /// aca ingresa si el dato es incorrecto
+            flag=1;
+        }
+    }
+    return flag;
+}
+
+
+int validarDni(char nombreArchivo[], char dni[])
+{
+
+
+    if (validarNumero(dni) != 0)
+    {
+        printf("\n DNI no valido, vuelva a ingresarlo ");
+        return 1;
+    }
+
+    FILE *archivo = fopen(nombreArchivo, "rb");
+    stCliente c;
+    if(archivo)
+    {
+        while(fread(&c, sizeof(stCliente), 1, archivo) > 0)
+        {
+            if(strcmp(c.dni, dni) == 0)
+            {
+                ///yellow();
+                printf("\nEste dni ya se encuentra registrado\n");
+                printf("Intenta con otro dni\n");
+                ///white();
+                return 1;
+            }
+        }
+        fclose(archivo);
+    }
+    return 0;
+}
+
+int validarMovil(char nombreArchivo[], char movil[])
+{
+
+    if (validarNumero(movil) != 0)
+    {
+        printf("\n Numero no valido, vuelva a ingresarlo ");
+        return 1;
+    }
+}
+
+int validarNroCliente(char nombreArchivo[], int nroCliente)
+{
+
+    if (validarUnEntero(nroCliente) != 0)
+    {
+        printf("\n Numero incorrecto, vuelva a ingresar en el rango de 1-999 ");
+        return 1;
+    }
+
+    FILE *archivo = fopen(nombreArchivo, "rb");
+    stCliente c;
+    if(archivo)
+    {
+        while(fread(&c, sizeof(stCliente), 1, archivo) > 0)
+        {
+            if(c.nroCliente == nroCliente)
+            {
+                ///yellow();
+                printf("\n Este numero de cliente ya se encuentra registrado\n");
+                printf("Intenta con otro numero \n");
+                ///white();
+                return 1;
+            }
+        }
+        fclose(archivo);
+    }
+    return 0;
+}
+
+int validarUnEntero(int num)
+{
+    int flag = 0;
+    int i=0;
+    while (flag == 0 && i==0)
+    {
+        if ((num<0) || (num>1000))
+    {
+        /// printf("\n Numero incorrecto, vuelva a ingresar en el rango de 1-999");
+        flag = 1;
+        }
+        i++;
+    }
+    return flag;
+}
+
+
