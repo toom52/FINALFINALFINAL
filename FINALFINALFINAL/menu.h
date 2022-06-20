b7ad189a016b846637e1c81be41b9456024d8dcd
Jeremias Canedo <104740812+jerecane@users.noreply.github.com>
2022-06-20 20:29:39 -0300

    Add files via upload

---
+++ FINALFINALFINAL/menu.h
@@ -0,0 +1,23 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+
+/// MENU PRINCIPAL
+void menuPrincipal();
+
+/// MENU CLIENTES
+void menuClientes();
+
+/// MENU PARA IMPRIMIR CLIENTES
+void imprimirClientes();
+
+///MENU DE BUSQUEDA DE CLIENTES
+void menuBusquedaCliente();
+
+///MENU DE CONSUMOS DE CLIENTES
+void menuConsumos();
+
+/// MENU A-B CLIENTE
+/// ME FALTAN LAS FUNCIONES PARA ESTE MENU
+void altaBajaMenu();
+
+#endif // MENU_H_INCLUDED
